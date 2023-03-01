4e284f06232c986f5e145f673e74f4c57a548ce8
Denilson <dlemus@harding.edu>
2023-03-01 16:35:11 -0600

    💄 Add tailwing utilities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        Home
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
   );
