f0a0a7f0b1319600b3fd0256cc635a0ed34e1d06
Denilson <dlemus@harding.edu>
2023-03-02 16:29:03 -0600

    ✨ Add top onboarding section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import Image from "next/image";
 import { Inter } from "@next/font/google";
+import { IconButton } from "@material-tailwind/react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Header from "@/components/Header/Header";
+import newDoc from "@/assets/new.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +16,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header />
-      <main>
+      <main className="bg-gray-200 pb-10 px-10">
+        <div className="max-w-5xl mx-auto">
+          <div className="flex items-center justify-between pt-6 pb-2">
+            <h2 className="text-gray-900">Start a new document</h2>
+            <IconButton
+              className="rounded rounded-full border-transparent focus:ring-0 p-6 text-gray-700"
+              color="gray"
+              variant="outlined"
+              ripple={true}
+            >
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+          <div>
+            <div className="relative h-40 w-32 border-[1px] cursor-pointer border-gray-300 rounded-md hover:border-blue-500">
+              <Image className="rounded-md" src={newDoc} fill={true} alt="" />
+            </div>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+          </div>
+        </div>
       </main>
     </>
   );
