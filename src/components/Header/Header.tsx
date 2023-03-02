f891a8ebd4cd291900b9447b1bf08c0723ae2ff9
Denilson <dlemus@harding.edu>
2023-03-02 13:03:55 -0600

    🚧 Work on search bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { IconButton } from "@material-tailwind/react";
 
 function Header() {
@@ -11,9 +11,13 @@ function Header() {
         variant="outlined"
         ripple={true}
       >
-        <FontAwesomeIcon size="2xl" icon={faBars} />
+        <FontAwesomeIcon size="xl" icon={faBars} />
       </IconButton>
       <h1>Google Does</h1>
+      <div className="mx-5 md:mx-20 flex flex-grow items-center px-4 py-2 bg-gray-200 text-gray-700 rounded-md focus-within:shadow-md">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <input className="flex-grow px-4 text-base bg-transparent outline-none" type="text" placeholder="Search" />
+      </div>
     </header>
   );
 }
