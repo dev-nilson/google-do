6c2139757cf5609719a5e9c4ad426734b24feb0b
Denilson <dlemus@harding.edu>
2023-03-07 18:35:53 -0600

    🔧 Configure firebase

---
+++ firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "dev-nilson--does.firebaseapp.com",
+  projectId: "dev-nilson--does",
+  storageBucket: "dev-nilson--does.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+export { db };
