fed61ea72f64ff581dcb83b7ad4ef2c45c6ff910
Denilson <dlemus@harding.edu>
2023-03-09 12:23:38 -0600

    🥅 Handle untitled document

--- src/pages/document/index.tsx
+++ src/pages/document/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
@@ -6,14 +7,40 @@ import SaveIcon from "@mui/icons-material/Save";
 import TextEditor from "@/components/TextEditor/TextEditor";
 
 function DocPage() {
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (!title) {
+      inputRef.current?.focus();
+      setError("Name document before saving");
+    } else {
+      setError("");
+    }
+  };
+
   return (
     <div>
       <header className="flex justify-between items-center px-5 pt-3 pb-1">
         <Link href="/">
           <Image src={logo} alt="logo" width={25} height={25} />
         </Link>
-        <div className="flex-grow ml-3">
-          <h2>Untitled Document</h2>
+        <div className="flex flex-col flex-grow ml-3">
+          <div>
+            <input
+              className="w-fit px-2"
+              placeholder="Untitled Document"
+              ref={inputRef}
+              value={title}
+              onChange={handleChange}
+            />
+            <small className="ml-2 text-red-500">{error}</small>
+          </div>
           <div className="items-center text-sm space-x-1 text-gray-800 hidden sm:inline-flex">
             <p className="option">File</p>
             <p className="option">Edit</p>
@@ -24,8 +51,13 @@ function DocPage() {
           </div>
         </div>
 
-        <Button className="flex items-center gap-1" size="sm">
-          <SaveIcon fontSize="small" />Save
+        <Button
+          className="flex items-center gap-1"
+          size="sm"
+          onClick={handleClick}
+        >
+          <SaveIcon fontSize="small" />
+          Save
         </Button>
       </header>
       <TextEditor />
