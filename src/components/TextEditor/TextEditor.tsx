5ff2756ef7cfcf040918753bacb46cab0a21452a
Denilson <dlemus@harding.edu>
2023-03-08 19:32:12 -0600

    ✨ Handle editor state

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import dynamic from "next/dynamic";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((module) => module.Editor),
@@ -9,11 +11,19 @@ const Editor = dynamic(
 );
 
 function TextEditor() {
+  const [editorState, seteditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState: any) => {
+    seteditorState(editorState);
+    console.log(convertToRaw(editorState.getCurrentContent()).blocks);
+  };
+
   return (
     <div className="bg-gray-200 min-h-screen pb-16">
       <Editor
         toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
         editorClassName="mt-6 p-10 bg-white shadow-md max-w-4xl mx-auto"
+        onEditorStateChange={onEditorStateChange}
       />
     </div>
   );
