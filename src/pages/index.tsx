e7b1f93e069cdfba814b2589f0658fc5536350a1
Denilson <dlemus@harding.edu>
2023-04-01 00:54:49 -0600

    ✨ Set favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <Head>
         <title>Google Does</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>
