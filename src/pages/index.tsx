7f4d829814e9ca5d0cfffa50b047cb93fab869dd
Denilson <dlemus@harding.edu>
2023-04-04 17:23:29 -0600

    ✨ Add document card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,10 +53,15 @@ export default function Home() {
         </section>
         <section className="bg-white px-10">
           <div className="max-w-5xl mx-auto py-8">
-            <div className="flex items-center justify-between text-gray-700 text-sm">
+            <div className="flex items-center justify-between text-gray-700 text-sm mb-5">
               <h2 className="font-semibold flex-grow">Documents</h2>
               <p className="font-semibold mr-12">Date</p>
               <FolderIcon />
+            </div>
+            <div className="flex items-center justify-between text-gray-700 text-sm">
+              <h2 className="font-medium flex-grow">Title</h2>
+              <p className="font-medium mr-12">April 4, 2023</p>
+              <MoreVertIcon />
             </div>
           </div>
         </section>
