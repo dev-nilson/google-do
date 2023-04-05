c33f1b6bcd6d8281bcaa8fd271913a8afcb5e18c
Denilson <dlemus@harding.edu>
2023-04-05 12:30:57 -0600

    ✨ Read doc from explorer

--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import FolderIcon from "@mui/icons-material/Folder";
 
 function Explorer() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const docString = localStorage.getItem("doc");
+
+    if (docString) {
+      const doc = JSON.parse(docString);
+      setTitle(doc.title);
+      setDate(doc.date);
+    }
+  }, []);
+
   return (
     <section className="bg-white px-10">
       <div className="max-w-5xl mx-auto py-8">
@@ -11,8 +25,8 @@ function Explorer() {
           <FolderIcon />
         </div>
         <div className="flex items-center justify-between text-gray-700 text-sm">
-          <h2 className="font-medium flex-grow">Title</h2>
-          <p className="font-medium mr-12">April 4, 2023</p>
+          <h2 className="font-medium flex-grow">{title}</h2>
+          <p className="font-medium mr-12">{date}</p>
           <MoreVertIcon />
         </div>
       </div>
